d559fe2d3479c9ff9d6d5815bede3d77af8d488b
aza <aza@tabby.ing>
2024-12-19 11:33:31 +0100

    idgaf

---
+++ modules/bruhman/yapper.go
@@ -0,0 +1,22 @@
+package bruhman
+
+import (
+        "fmt"
+	"code.gitea.io/gitea/modules/markup"
+	"code.gitea.io/gitea/modules/markup/markdown"
+)
+
+// Render takes a markdown string and returns the rendered HTML string or an error.
+func Render(markdownContent string) (string, error) {
+	buffer, err := markdown.RenderString(&markup.RenderContext{
+		Ctx: nil,
+		Links: markup.Links{
+			Base: "http://localhost:3000/user13/repo11/",
+		},
+	}, markdownContent)
+	if err != nil {
+		return "", err
+	}
+
+	return string(buffer), nil
+}
